70ba60f8eef99a2cd175bc722b7c66927f052e7c
Mohit Khairnar <mohitkhairnar99@gmail.com>
2024-06-17 15:59:12 +0530

    commit msg

---
+++ Employe Management System/Interface/IEmployeeAdditionalDetails.cs
@@ -0,0 +1,18 @@
+﻿using Employee_Management_System.DTO;
+
+namespace Employee_Management_System.Interface
+{
+    public interface IEmployeeAdditionalDetails
+    {
+        Task<EmployeeAdditionalDetailsDTO> Add_AdditionalData(EmployeeAdditionalDetailsDTO employeeDetails);
+        Task<List<EmployeeAdditionalDetailsDTO>> GetAllEmployeeAdditionalData();
+
+        Task<EmployeeAdditionalDetailsDTO> GetEmployeeAdditionalDataByUId(string uId);
+
+        Task<EmployeeAdditionalDetailsDTO> UpdateAdditionalData(EmployeeAdditionalDetailsDTO employeeDetails);
+
+        Task<string> DeleteEmployeeAdditional(string UId);
+
+       
+    }
+}
